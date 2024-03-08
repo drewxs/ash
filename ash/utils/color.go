3536f4f5ebf38f34dffc53aa4d78764da45a15ca
Andrew X. Shah <drewshah0@gmail.com>
2024-03-07 23:06:09 -0700

    feat: create color module

---
+++ ash/utils/color.go
@@ -0,0 +1,19 @@
+package color
+
+import "fmt"
+
+const (
+	NONE    = 37
+	BLACK   = 30
+	RED     = 31
+	GREEN   = 32
+	YELLOW  = 33
+	BLUE    = 34
+	MAGENTA = 35
+	CYAN    = 36
+	WHITE   = 37
+)
+
+func Format(c int, text string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", c, text)
+}
