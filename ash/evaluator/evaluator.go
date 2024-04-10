a1e8e5594540d4a22f2e86bc65f0642ce571b11e
Andrew X. Shah <drewshah0@gmail.com>
2024-04-09 22:54:32 -0600

    feat(evaluation): add string eval

--- ash/evaluator/evaluator.go
+++ ash/evaluator/evaluator.go
@@ -20,11 +20,14 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression, env)
 
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
-	case *ast.Boolean:
-		return nativeBoolToBooleanObject(node.Value)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 
 	case *ast.PrefixExpression:
 		right := Eval(node.Right, env)
--- ash/object/object.go
+++ ash/object/object.go
@@ -10,17 +10,31 @@ import (
 type ObjectType string
 
 const (
-	INTEGER_OBJ      = "INTEGER"
-	BOOLEAN_OBJ      = "BOOLEAN"
-	NULL_OBJ         = "NULL"
-	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJ        = "ERROR"
+	BOOLEAN_OBJ = "BOOLEAN"
+	INTEGER_OBJ = "INTEGER"
+	STRING_OBJ  = "STRING"
+
+	NULL_OBJ  = "NULL"
+	ERROR_OBJ = "ERROR"
+
 	FUNCTION_OBJ     = "FUNCTION"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type String struct {
+	Value string
 }
 
 type Integer struct {
@@ -30,12 +44,8 @@ type Integer struct {
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
-type Boolean struct {
-	Value bool
-}
-
-func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
 
 type Null struct{}
 
