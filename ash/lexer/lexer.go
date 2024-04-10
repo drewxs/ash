b5586f9748fd4c6f4ca2fb0cabcd41aabb0f26c5
Andrew X. Shah <drewshah0@gmail.com>
2024-04-09 22:34:55 -0600

    feat(lexer): support strings

--- ash/lexer/lexer.go
+++ ash/lexer/lexer.go
@@ -63,6 +63,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.SEMICOLON, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -110,6 +113,17 @@ func (l *Lexer) readNum() string {
 	position := l.position
 	for isDigit(l.ch) {
 		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
 	}
 	return l.input[position:l.position]
 }
