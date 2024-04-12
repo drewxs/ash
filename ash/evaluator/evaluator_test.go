84eb68416bea5375fd74bf1a1e98bcf20134d66d
Andrew X. Shah <drewshah0@gmail.com>
2024-04-12 03:25:29 -0600

    feat(evaluator): add array indexing eval

--- ash/evaluator/evaluator_test.go
+++ ash/evaluator/evaluator_test.go
@@ -347,6 +347,64 @@ func TestArrayLiterals(t *testing.T) {
 	testIntegerObject(t, result.Elements[2], 6)
 }
 
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			"[1, 2, 3][0]",
+			1,
+		},
+		{
+			"[1, 2, 3][1]",
+			2,
+		},
+		{
+			"[1, 2, 3][2]",
+			3,
+		},
+		{
+			"let i = 0; [1][i]",
+			1,
+		},
+		{
+			"[1, 2, 3][1 + 1]",
+			3,
+		},
+		{
+			"let arr = [1, 2, 3]; arr[2];",
+			3,
+		},
+		{
+			"let arr = [1, 2, 3]; arr[0] + arr[1] + arr[2];",
+			6,
+		},
+		{
+			"let arr = [1, 2, 3]; let i = arr[0]; arr[i]",
+			2,
+		},
+		{
+			"[1, 2, 3][3]",
+			nil,
+		},
+		{
+			"[1, 2, 3][-1]",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
