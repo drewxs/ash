4c9d1f9ba4ee0037ad553cc62234cfb2aeab754e
Andrew X. Shah <drewshah0@gmail.com>
2024-04-19 00:44:58 -0600

    feat: add hash indexing

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -185,6 +185,10 @@ func TestErrorHandling(t *testing.T) {
 			`"Hello" - "World"`,
 			"unknown operator: STRING - STRING",
 		},
+		{
+			`{"name": "Ash"}[fn(x) { x }];`,
+			"unusable as hash key: FUNCTION",
+		},
 	}
 
 	for _, tt := range tests {
@@ -352,50 +356,17 @@ func TestArrayIndexExpressions(t *testing.T) {
 		input    string
 		expected interface{}
 	}{
-		{
-			"[1, 2, 3][0]",
-			1,
-		},
-		{
-			"[1, 2, 3][1]",
-			2,
-		},
-		{
-			"[1, 2, 3][2]",
-			3,
-		},
-		{
-			"let i = 0; [1][i]",
-			1,
-		},
-		{
-			"[1, 2, 3][1 + 1]",
-			3,
-		},
-		{
-			"let arr = [1, 2, 3]; arr[2];",
-			3,
-		},
-		{
-			"let arr = [1, 2, 3]; arr[0] + arr[1] + arr[2];",
-			6,
-		},
-		{
-			"let arr = [1, 2, 3]; let i = arr[0]; arr[i]",
-			2,
-		},
-		{
-			"[1, 2, 3][3]",
-			nil,
-		},
-		{
-			"[1, 2, 3][-1]",
-			3,
-		},
-		{
-			"[1, 2, 3][-3]",
-			1,
-		},
+		{"[1, 2, 3][0]", 1},
+		{"[1, 2, 3][1]", 2},
+		{"[1, 2, 3][2]", 3},
+		{"let i = 0; [1][i]", 1},
+		{"[1, 2, 3][1 + 1]", 3},
+		{"let arr = [1, 2, 3]; arr[2];", 3},
+		{"let arr = [1, 2, 3]; arr[0] + arr[1] + arr[2];", 6},
+		{"let arr = [1, 2, 3]; let i = arr[0]; arr[i]", 2},
+		{"[1, 2, 3][3]", nil},
+		{"[1, 2, 3][-1]", 3},
+		{"[1, 2, 3][-3]", 1},
 	}
 
 	for _, tt := range tests {
@@ -446,6 +417,31 @@ func TestHashLiteral(t *testing.T) {
 		}
 
 		testIntegerObject(t, pair.Value, expectedValue)
+	}
+}
+
+func TestHashIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{`{"foo": 5}["foo"]`, 5},
+		{`{"foo": 5}["bar"]`, nil},
+		{`let key = "foo"; {"foo": 5}[key]`, 5},
+		{`{}["foo"]`, nil},
+		{`{5: 5}[5]`, 5},
+		{`{true: 5}[true]`, 5},
+		{`{false: 5}[false]`, 5},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
 	}
 }
 
