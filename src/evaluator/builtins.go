0e9bcc30f0d9c200db5c3b4b32284774ec805908
Andrew X. Shah <drewshah0@gmail.com>
2024-04-19 00:49:28 -0600

    feat(builtins): add print

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"ash/object"
+	"fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -97,6 +98,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"print": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
