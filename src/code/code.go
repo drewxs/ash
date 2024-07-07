530b6d0fa202a280d5773ce5360a68d096913deb
Andrew X. Shah <drewshah0@gmail.com>
2024-07-07 04:23:00 -0600

    feat(code): create local binding opcodes

--- src/code/code.go
+++ src/code/code.go
@@ -29,9 +29,6 @@ const (
 	OpJumpNotTruthy
 	OpJump
 
-	OpGetGlobal
-	OpSetGlobal
-
 	OpArray
 	OpHash
 	OpIndex
@@ -39,6 +36,12 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+
+	OpGetGlobal
+	OpSetGlobal
+
+	OpGetLocal
+	OpSetLocal
 )
 
 type Instructions []byte
@@ -73,9 +76,6 @@ var definitions = map[Opcode]*Definition{
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
 
-	OpGetGlobal: {"OpGetGlobal", []int{2}},
-	OpSetGlobal: {"OpSetGlobal", []int{2}},
-
 	OpArray: {"OpArray", []int{2}},
 	OpHash:  {"OpHash", []int{2}},
 	OpIndex: {"OpIndex", []int{}},
@@ -83,6 +83,12 @@ var definitions = map[Opcode]*Definition{
 	OpCall:        {"OpCall", []int{1}},
 	OpReturnValue: {"OpReturnValue", []int{}},
 	OpReturn:      {"OpReturn", []int{}},
+
+	OpGetGlobal: {"OpGetGlobal", []int{2}},
+	OpSetGlobal: {"OpSetGlobal", []int{2}},
+
+	OpGetLocal: {"OpGetLocal", []int{1}},
+	OpSetLocal: {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -114,6 +120,8 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		offset += width
 	}
@@ -150,12 +158,18 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 
 		offset += width
 	}
 
 	return operands, offset
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func ReadUint16(ins Instructions) uint16 {
